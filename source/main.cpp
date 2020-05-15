78a173985f36975454372b35549d3a873eb8af6f
progrm_jarvis <gh@progrm-jarvis.ru>
2020-05-15 05:28:24 +0300

    Implement logic of main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 
 #include <boost/multiprecision/cpp_int.hpp>
-#include <operation/algebraic_operations.h>
-#include <operation/const_operation.h>
-#include <operation/variables.h>
 #include <parser/simple_expression_parser.h>
 
 using BigDecimal = boost::multiprecision::cpp_rational;
-// TODO remove
+
+static const size_t MAX_VARIABLES = 10;
+
+Variables<BigDecimal> readVariables();
 
 int main() {
-    calculator::PrimitiveSqrtOperation<int> expr(std::make_shared<calculator::PlusOperation<int>>(
-            std::make_shared<calculator::DivideOperation<int>>(std::make_shared<calculator::ConstOperation<int>>(4),
-                                                               std::make_shared<calculator::ConstOperation<int>>(2)),
-            std::make_shared<calculator::ConstOperation<int>>(7)));
-
-    Variables<int> variables(std::map<char, int>({{'x', 12}}));
-
-    std::cout << "OperationResult = " << expr.result(variables) << std::endl;
     calculator::SimpleOperationParser<BigDecimal> parser;
 
-    auto in = std::stringstream("1+2+3434+34*4*22-32434-(12*3+4-5)-2*3+4");
-    //std::string expression;
-    //getline(std::cin, expression);
-    Variables<BigDecimal> variablesBD(std::map<char, BigDecimal>({{'x', 12}}));
-    std::cout << parser.parse(in)->result(variablesBD) << std::endl;
-
-    std::cout << "Hello, World!" << std::endl;
+    std::string input;
+    do {
+        auto const variables = readVariables();
+        try {
+            std::cout << "Enter the expression to calculate" << std::endl;
+            std::cin.ignore();
+            auto const parsed = parser.parse(std::cin);
+            try {
+                auto const result = parsed->result(variables);
+                std::cout << "\t=\t" << result << std::endl;
+            } catch (calculator::OperationError const& e) {
+                std::cerr << "Operation error: " << e.what() << std::endl;
+            }
+        } catch (calculator::InvalidExpression const& e) {
+            std::cerr << "Invalid expression: " << e.what() << std::endl;
+        }
+        std::cout << "Enter `end`, `exit` or `stop` to end this session" << std::endl;
+        getline(std::cin, input);
+    } while (input != "end" && input != "exit" && input != "stop");
     return 0;
+}
+
+Variables<BigDecimal> readVariables() {
+    size_t count;
+    do {
+        std::cout << "Enter the number of variables you would like to use (max: " << MAX_VARIABLES << ')' << std::endl;
+        std::cin >> count;
+    } while (count > MAX_VARIABLES);
+
+    std::map<char, BigDecimal> variables;
+    for (size_t i = 0; i < count; ++i) {
+        char name;
+        BigDecimal value;
+        do {
+            std::cout << "Enter the name of the variable and its value" << std::endl;
+            std::cin >> name >> value;
+        } while (variables.contains(name));
+    }
+
+    return Variables(variables);
 }
